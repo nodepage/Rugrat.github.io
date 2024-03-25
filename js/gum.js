449c0be7bf7c49496acbc0505ab3552e85dc86cb
Your Name <you@example.com>
2024-03-25 05:44:53 -0700

    update

--- js/gum.js
+++ js/gum.js
@@ -1,49 +1,43 @@
-var xClickz = document.getElementById('xicon');
+var xClickz = document.getElementById("xicon");
 let tClickz = document.getElementById("ticon");
-var btn1 = document.getElementsByClassName('btn1')[0]
-var btn2 = document.getElementsByClassName('btn2')[0]
+var btn1 = document.getElementsByClassName("btn1")[0];
+var btn2 = document.getElementsByClassName("btn2")[0];
 
-var retweetz = false
-var teleConf = false
-var addConf = false
-
+var retweetz = false;
+var teleConf = false;
+var addConf = false;
+var referrals = 0;
 
 var xClickedz = false;
 var tClickedz = false;
 
 xClickz.addEventListener("click", function () {
-  
-
-  xClickedz = true
-  xWarn.style.display ='none'
-  btn1.style.color = "#4e5863"
+  xClickedz = true;
+  xWarn.style.display = "none";
+  btn1.style.color = "#4e5863";
 });
 
 tClickz.addEventListener("click", function () {
-    
+  tClickedz = true;
+  tWarn.style.display = "none";
+  btn2.style.color = "#4e5863";
+});
 
-    tClickedz = true
-    tWarn.style.display ='none'
-    btn2.style.color = "#4e5863"
+var twitterUrl = document.getElementById("id_quote_retweet_link").value;
+var xWarn = document.getElementById("xfollw-warning");
+var tWarn = document.getElementById("twarning");
+var airdropBtn = document.getElementById("airdropsubmitbtn");
+var retweetWarn = document.getElementById("xwarning");
 
-  });
-
-
-  var twitterUrl = document.getElementById('id_quote_retweet_link').value
-  var xWarn = document.getElementById('xfollw-warning')
-  var tWarn = document.getElementById('twarning')
-  var airdropBtn = document.getElementById('airdropsubmitbtn')
-  var retweetWarn = document.getElementById('xwarning')
-
-  var telUserWarn = document.getElementById('tuserWarning')
-
-
+var telUserWarn = document.getElementById("tuserWarning");
+var clearTask = document.getElementById("taskz");
+var closeTask = document.getElementById("mover");
+var Giveaway = document.getElementById('givez')
 
 //   airdropBtn.addEventListener("click", function (event) {
 //     event.preventDefault()
-    
+
 //     if(xClickedz ){
-       
 
 //         if(tClickedz){
 
@@ -56,20 +50,17 @@ tClickz.addEventListener("click", function () {
 //                 //     telUserWarn.innerHTML = 'Please input a valid username'
 //                 // }
 
-               
 //             }else{
 //                 retweetWarn.innerHTML = "Please input a valid link"
 
-                
 //             }
 //         }else{
 //             tWarn.innerHTML = 'You have not Join our Telegram community'
 //         }
 
-        
 //     }else{
 //         xWarn.innerHTML='ERROR! You have not Follow us on Twitter'
-       
+
 //     }
 
 //     if(retweetz){
@@ -77,24 +68,20 @@ tClickz.addEventListener("click", function () {
 //     }
 //   });
 
-
 // airdropBtn.addEventListener("click", function (event) {
 //     event.preventDefault()
 //     var retweetUrl = document.getElementById('id_quote_retweet_link').value.length
 //     var telUsername = document.getElementById('id_telegram_username').value
 //     var addValue = document.getElementById('id_solana_wallet').value
 //     var addWarn = document.getElementById('addwarning')
-    
-  
+
 //     if(xClickedz ){
-       
 
 //         if( retweetUrl > 40 ){
-           
+
 //            retweetz = true
 
 //             if (tClickedz){
-                
 
 //                 if(telUsername.length > 3){
 //                     teleConf = true
@@ -110,11 +97,9 @@ tClickz.addEventListener("click", function () {
 //                     telUserWarn.innerHTML = 'Please Enter a valid username'
 //                 }
 
-               
 //             }else{
 //                 tWarn.innerHTML = 'You have not Join our Telegram community'
-                
-                
+
 //             }
 //         }else{
 //             retweetWarn.innerHTML = "Please Enter a valid link"
@@ -122,101 +107,146 @@ tClickz.addEventListener("click", function () {
 
 //         }
 
-        
 //     }else{
 //         xWarn.innerHTML='ERROR! You have not Follow us on Twitter'
-       
-//     }
 
+//     }
 
 //     if(retweetz){retweetWarn.style.display = "none"}
 //     if(teleConf){telUserWarn.style.display = "none"}
 //     if(addConf){addWarn.style.display = "none"}
 
-
 //   });
 
-
 // Add event listeners to input fields for validation on change
-document.getElementById('id_quote_retweet_link').addEventListener('input', function () {
+document
+  .getElementById("id_quote_retweet_link")
+  .addEventListener("input", function () {
     validateInputs();
-});
+  });
 
-document.getElementById('id_telegram_username').addEventListener('input', function () {
+document
+  .getElementById("id_telegram_username")
+  .addEventListener("input", function () {
     validateInputs();
-});
+  });
 
-document.getElementById('id_solana_wallet').addEventListener('input', function () {
+document
+  .getElementById("id_solana_wallet")
+  .addEventListener("input", function () {
     validateInputs();
-});
+  });
 
 // Event listener for the airdropBtn click event
-airdropBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-    validateInputs();
+airdropBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  validateInputs();
 });
 
 // Function to validate inputs
 function validateInputs() {
-    var retweetUrl = document.getElementById('id_quote_retweet_link').value.trim();
-    var telUsername = document.getElementById('id_telegram_username').value.trim();
-    var addValue = document.getElementById('id_solana_wallet').value.trim();
-    var addWarn = document.getElementById('addwarning');
+  var retweetUrl = document
+    .getElementById("id_quote_retweet_link")
+    .value.trim();
+  var telUsername = document
+    .getElementById("id_telegram_username")
+    .value.trim();
+  var addValue = document.getElementById("id_solana_wallet").value.trim();
+  var addWarn = document.getElementById("addwarning");
 
-    var retweetWarn = document.getElementById('xwarning');
-    var tWarn = document.getElementById('twarning');
-    var xWarn = document.getElementById('xfollw-warning');
-    var telUserWarn = document.getElementById('tuserWarning');
+  var retweetWarn = document.getElementById("xwarning");
+  var tWarn = document.getElementById("twarning");
+  var xWarn = document.getElementById("xfollw-warning");
+  var telUserWarn = document.getElementById("tuserWarning");
 
-    var retweetz = false;
-    var teleConf = false;
-    var addConf = false;
+  var retweetz = false;
+  var teleConf = false;
+  var addConf = false;
 
-    if(!xClickedz ){
-        xWarn.innerHTML='ERROR! You have not Follow us on Twitter'
-    }
+  if (!xClickedz) {
+    xWarn.innerHTML = "ERROR! You have not Follow us on Twitter";
+  }
 
-    
-    
+  if (retweetUrl.length > 40) {
+    retweetz = true;
+    retweetWarn.style.display = "none";
+  } else {
+    retweetz = false;
+    retweetWarn.innerHTML = "Please enter a valid link";
+    retweetWarn.style.display = "block";
+  }
 
-    if (retweetUrl.length > 40) {
-        retweetz = true;
-        retweetWarn.style.display = 'none';
+  if (!tClickedz) {
+    tWarn.innerHTML = "You have not Join our Telegram community";
+  }
+
+  if (telUsername.length > 3) {
+    teleConf = true;
+    telUserWarn.style.display = "none";
+  } else {
+    teleConf = false;
+    telUserWarn.innerHTML = "Please enter a valid username";
+    telUserWarn.style.display = "block";
+  }
+
+  if (addValue.length > 30) {
+    addConf = true;
+    addWarn.style.display = "none";
+  } else {
+    addConf = false;
+    addWarn.innerHTML = "Please enter a valid address";
+    addWarn.style.display = "block";
+  }
+
+  // Check if all validations pass
+  if (xClickedz && tClickedz && retweetz && teleConf && addConf) {
+    // Proceed with your logic here
+    //addValue is the Address number
+
+    Giveaway.innerHTML = "Successfully Participated"
+    clearTask.style.display = "none";
+    closeTask.style.display = "none";
+
+    let paragraph = document.createElement("p");
+
+    let strongElement = document.createElement("strong");
+    strongElement.textContent = "Referrals ID:";
+    paragraph.appendChild(strongElement);
+
+    paragraph.appendChild(document.createTextNode(" SSSSSSS"));
+
+    paragraph.setAttribute("id", "success");
+
+    let form = document.getElementById("airdrop-form");
+
+    form.appendChild(paragraph);
+
+
+    let Newparagraph = document.createElement("p");
+    var old = document.getElementById("success");
+    let strongElement2 = document.createElement("strong");
+    strongElement2.textContent = "RAT BALANCE :";
+    Newparagraph.appendChild(strongElement2);
+    Newparagraph.appendChild(document.createTextNode(" 5000 $RAT"));
+    Newparagraph.setAttribute("id", "ratu");
+    old.appendChild(Newparagraph);
+
+
+
+    // Append the paragraph to the body of the document
+
+    if (false) {
+      //featch data
     } else {
-        retweetz = false;
-        retweetWarn.innerHTML = 'Please enter a valid link';
-        retweetWarn.style.display = 'block';
-    }
+      var data = {
+        address: addValue,
+        telegram: telUsername,
+        referrals: referrals,
+      };
 
-    if(!tClickedz){
-        tWarn.innerHTML = 'You have not Join our Telegram community'
-    }
+      let jsonString = JSON.stringify(data); //Json covert data
 
-    if (telUsername.length > 3) {
-        teleConf = true;
-        telUserWarn.style.display = 'none';
-    } else {
-        teleConf = false;
-        telUserWarn.innerHTML = 'Please enter a valid username';
-        telUserWarn.style.display = 'block';
+      localStorage.setItem("Data", jsonString);
     }
-
-    if (addValue.length > 30) {
-        addConf = true;
-        addWarn.style.display = 'none';
-    } else {
-        addConf = false;
-        addWarn.innerHTML = 'Please enter a valid address';
-        addWarn.style.display = 'block';
-    }
-
-    // Check if all validations pass
-    if (xClickedz && tClickedz && retweetz && teleConf && addConf) {
-        // Proceed with your logic here
-       if(){
-
-       }else{
-        
-       }
-    }
+  }
 }
